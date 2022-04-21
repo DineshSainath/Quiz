406950fd9d428cd7c745530d4c5235fa9ac271e5
Dinesh Sainath <sainath.dinesh@gmail.com>
2022-04-21 17:46:43 -0400

    select image - border/shadow

---
+++ quiz/src/Images.jsx
@@ -0,0 +1,35 @@
+let imgs = [
+    {
+        id: "1",
+        name: "Film",
+        src: "https://images.ctfassets.net/3s5io6mnxfqz/3qYlrsjfuRzOrGBvGW3iog/1028a32ba6d03297f9f0c6e337ade965/AdobeStock_113882591.jpeg?fm=jpg&w=900&fl=progressive" 
+    },
+    {
+        id: "2",
+        name: "Television", 
+        src: "https://media.istockphoto.com/photos/an-old-tv-with-a-monochrome-picture-id1159377900?k=20&m=1159377900&s=612x612&w=0&h=UTB9D0IMHbjdoZB6dDUFs_9Q_lCGAGDKNwe2EVuRZPk="
+    },
+    {
+        id: "3",
+        name: "Mathematics", 
+        src: "https://www.usnews.com/dims4/USNEWS/9b4381c/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F16%2F8a%2F7f6cb88e4a4bae4b0dd576654aa1%2Fcomplexmathequation.jpg"
+    },
+    {
+        id: "4",
+        name: "Nature", 
+        src: "https://cdn-prod.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg"
+    },
+    
+    {
+        id: "5",
+        name: "Computers", 
+        src: "https://i.insider.com/5d77cedd6f24eb491118e643?width=750&format=jpeg&auto=webp"
+    },
+    {
+        id: "6",
+        name: "Politics", 
+        src: "https://thebasilisk.org/wp-content/uploads/2021/03/frieden-1600.jpg"
+    },
+];
+
+export default imgs;
--- quiz/src/Selection.jsx
+++ quiz/src/Selection.jsx
@@ -1,54 +1,24 @@
 import "./Selection.css";
 import React, {useState} from "react";
+import imgs from "./Images.jsx";
 
 function Selection(){
     
     const [category, setCategory] = useState("");
+    const [picIndex, setIndex] = useState();
 
-    let imgs = [
-        {
-            id: "1",
-            name: "Film",
-            src: "https://images.ctfassets.net/3s5io6mnxfqz/3qYlrsjfuRzOrGBvGW3iog/1028a32ba6d03297f9f0c6e337ade965/AdobeStock_113882591.jpeg?fm=jpg&w=900&fl=progressive" 
-        },
-        {
-            id: "2",
-            name: "Television", 
-            src: "https://media.istockphoto.com/photos/an-old-tv-with-a-monochrome-picture-id1159377900?k=20&m=1159377900&s=612x612&w=0&h=UTB9D0IMHbjdoZB6dDUFs_9Q_lCGAGDKNwe2EVuRZPk="
-        },
-        {
-            id: "3",
-            name: "Mathematics", 
-            src: "https://www.usnews.com/dims4/USNEWS/9b4381c/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F16%2F8a%2F7f6cb88e4a4bae4b0dd576654aa1%2Fcomplexmathequation.jpg"
-        },
-        {
-            id: "4",
-            name: "Nature", 
-            src: "https://cdn-prod.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg"
-        },
-        
-        {
-            id: "5",
-            name: "Computers", 
-            src: "https://i.insider.com/5d77cedd6f24eb491118e643?width=750&format=jpeg&auto=webp"
-        },
-        {
-            id: "6",
-            name: "Politics", 
-            src: "https://thebasilisk.org/wp-content/uploads/2021/03/frieden-1600.jpg"
-        },
-    ];
-
-    function clicked(value){
-        setCategory(value); 
+    function clicked(value, index){
+        setCategory(value);
+        setIndex(index);
+        console.log(index); 
     }
 
     return(<div>
     
         {/* pass value as a parameter for div-onClick */}
         {imgs.map((item,index) => (
-            <div className="images" onClick={() => clicked(item.name)} value={item.name}>
-        <img className="img" src={item.src} width="200" height="150"/>
+            <div className="images" key={index} onClick={() => clicked(item.name, index)} value={item.name}>
+        <img className={index === picIndex ? "imgSelected" : "img"} src={item.src} width="200" height="150"/>
             <div className="overlay">
                 <p className="desc"> {item.name} </p>
             </div>
@@ -58,6 +28,7 @@ function Selection(){
         <div>
             <p>You selected {category}</p>
         </div>
+        
 
     </div>)
 }
